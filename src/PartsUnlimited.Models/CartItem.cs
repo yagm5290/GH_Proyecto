00c40537265c753c31eb35e1ce89315cc6095421
yagm5290 <yagm5290@hotmail.com>
2024-05-16 09:26:08 -0500

    Primer repo

---
+++ src/PartsUnlimited.Models/CartItem.cs
@@ -0,0 +1,27 @@
+//Nuevo repositorio
+//Cambio 20240516 master 1 
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace PartsUnlimited.Models
+{
+    // Prueba Github 20240515 3:01 Otra
+    public class CartItem
+    {
+        [Key]
+        public int CartItemId { get; set; }
+
+        [Required]
+        public string CartId { get; set; }
+        public int ProductId { get; set; }
+        public int Count { get; set; }
+
+        [DataType(DataType.DateTime)]
+        public DateTime DateCreated { get; set; }
+
+        public virtual Product Product { get; set; }
+    }
+}
